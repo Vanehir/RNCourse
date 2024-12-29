350ef65385544ce01e5bd27a81cdcbeaa1a1e4c6
Vanehir <pietro.sotgiu@edu.itspiemonte.it>
2024-12-29 15:30:06 +0100

    used some "useCallBack"

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,16 @@
+import {useCallback, useState} from "react";
 import {Button, StyleSheet, Text, TextInput, View} from "react-native";
 
 export default function Index() {
+    const [enteredGoalText, setEnteredGoalText] = useState<string>('');
 
-    function
+    const goalInputHandler = useCallback((enteredText: string) => {
+        setEnteredGoalText(enteredText);
+    },[])
+
+    const addGoalHandler = useCallback(() => {
+        console.log(enteredGoalText);
+    },[enteredGoalText])
 
   return (
     <View style={styles.appContainer}>
@@ -10,8 +18,12 @@ export default function Index() {
             <TextInput
                 style={styles.textInput}
                 placeholder='you course goal'
+                onChangeText={goalInputHandler} // the function is not immediately executed here
             />
-            <Button title='add goal' onPress={() => {}}/>
+            <Button
+                title='add goal'
+                onPress={() => {addGoalHandler()}}
+            />
         </View>
         <View style={styles.goalsContainer}>
             <Text>list of goals...</Text>
